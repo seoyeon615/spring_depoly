6265befb1b39dc28547809fa50eea7b2f2ffe6f4
seoyeon615 <bsy070615@duksung.ac.kr>
2026-08-05 17:42:49 +0900

    테스트

--- src/main/java/net/likelion/backend/hello/HelloController.java
+++ src/main/java/net/likelion/backend/hello/HelloController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, World!";
+    @GetMapping("/hello2")
+    public String hello2(){
+        return "깃허브 액션 CICD 파이프라인 테스트";
     }
 }
